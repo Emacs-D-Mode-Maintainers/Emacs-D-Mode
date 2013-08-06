2cb1fe0f5d26a7f73a745506fc454f5af7252e41
finalpatch <fengli@gmail.com>
2013-08-07 00:09:54 +1000

    don't highlight []* in type face

--- d-mode.el
+++ d-mode.el
@@ -441,10 +441,10 @@ Key bindings:
 (font-lock-add-keywords
  'd-mode
  '(("\\<\\(auto\\|immutable\\)\\>" 1 font-lock-keyword-face)
-   ("^[ \t]*\\(?:[_a-zA-Z0-9]+[ \t\n]+\\)*\\([][_a-zA-Z0-9.*!]+\\)[ \t\n]+\\([_a-zA-Z0-9]+\\)[ \t\n]*;"
+   ("^[ \t]*\\(?:[_a-zA-Z0-9]+[ \t\n]+\\)*\\([_a-zA-Z0-9.!]+\\)[][* ]*[ \t\n]+\\([_a-zA-Z0-9]+\\)[ \t\n]*;"
     (1 font-lock-type-face)
     (2 font-lock-variable-name-face))
-   ("^[ \t]*\\(?:[_a-zA-Z0-9]+[ \t\n]+\\)*\\([][_a-zA-Z0-9.*!]+\\)[ \t\n]+\\([_a-zA-Z0-9]+\\)[ \t\n]*("
+   ("^[ \t]*\\(?:[_a-zA-Z0-9]+[ \t\n]+\\)*\\([_a-zA-Z0-9.!]+\\)[][* ]*[ \t\n]+\\([_a-zA-Z0-9]+\\)[ \t\n]*("
     (1 font-lock-type-face)
     (2 font-lock-function-name-face))))
 
