4f2994b1c4f89f580cc5a6227b8a0e5f894d818c
Dmitri Makarov <dmakarov@alumni.stanford.edu>
2015-04-09 17:13:12 +0200

    Add more code to the I0039 test.

--- tests/I0039.d
+++ tests/I0039.d
@@ -1,18 +1,54 @@
 void foo()
 {
-  version (a) {
-    // 
+  version (a)
+  {
   }
-  else version (b) {
-  }
-  else {
-  }
-
-  debug (A) {
-  }
-  else debug (B) {
+  else version (b)
+  {
   }
   else
+  {
+  }
+
+  debug (A)
+  {
+  }
+  else debug (B)
+  {
+  }
+  else
+  {
+  }
+
+  version (a)
+  {
+  }
+  else
+    version (b)
+    {
+    }
+    else
+    {
+    }
+
+  if (true)
+  {
+  }
+  else
+    if (true)
+    {
+    }
+    else
+    {
+    }
+
+  static if (1 < 2)
+  {
+  }
+  else static if (false)
+  {
+  }
+  else static if (true)
   {
   }
 }
