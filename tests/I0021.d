a9272644644ec339acd8b8c4e32b6ab4e10b85e2
Dmitri Makarov <dmakarov@alumni.stanford.edu>
2015-06-05 21:25:56 +0200

    Allow tests to specify lisp code and expected output.

--- tests/I0021.d
+++ tests/I0021.d
@@ -1,3 +1,4 @@
-// #run: (goto-char 74) (c-literal-limits)
-// #out: (72 . 77)
-auto x = `ab\`; // something else
+// #run: (let ((a (progn (goto-char 170) (c-literal-limits))) (b (progn (goto-char 230) (c-literal-limits)))) (list a b))
+// #out: ((167 . 172) (227 . 232))
+auto x = `ab\`; // back-quoted string ends with a backslash
+auto y = "c\""; // double-quoted string ends with an escaped "
