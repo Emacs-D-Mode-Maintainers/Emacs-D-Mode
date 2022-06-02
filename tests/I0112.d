024aca97d07e72bf3500fb6bf0cdf50c4992a741
Vladimir Panteleev <git@cy.md>
2022-06-02 06:49:33 +0000

    Add test for issue #112

---
+++ tests/I0112.d
@@ -0,0 +1,6 @@
+// #run: (progn (goto-char (- (point-max) 3)) (read-only-mode -1) (insert "\"") (run-hooks 'post-command-hook) (insert "\"") (font-lock-ensure) (d-test-fontification))
+
+void main()
+{
+	foo(
+}
