156450fa7a864e4cb92c18e4dd4ffd6a0fae0f63
Vladimir Panteleev <git@cy.md>
2024-02-18 17:48:49 +0000

    tests/I0112: Disable on Emacs 27.2
    Nobody cares about this specific version by now, probably.

--- tests/I0112.d
+++ tests/I0112.d
@@ -1,3 +1,4 @@
+// #condition: (or (version<= emacs-version "27.1") (version<= "28.0" emacs-version))
 // #run: (progn (goto-char (- (point-max) 3)) (read-only-mode -1) (insert "\"") (run-hooks 'post-command-hook) (insert "\"") (font-lock-ensure) (d-test-fontification))
 
 void main()
